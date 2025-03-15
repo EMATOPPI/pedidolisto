cc139a30ac043517ce4bf5b9ebcbb0fc27ce2456
Emanuel Toppi <toppiemanuel@gmail.com>
2025-03-15 16:28:02 -0300

    fix: actualizar configuración de Next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig 
